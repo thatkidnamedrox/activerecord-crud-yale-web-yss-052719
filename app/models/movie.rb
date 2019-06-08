cf46c1dbd7775f67ff24fa43833c51917d321896
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-08 08:19:25 +0000

    Automatically backed up by Learn

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
   def initialize(attributes)
-    attributes.each |key, value| { self.send("#{key}=", value) }
+    attributes.each { |key, value| self.send("#{key}=", value) }
   end
 end
