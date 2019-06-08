dfd72d0374ec209d4bd705a3ed1660c0dccd5e88
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-08 09:07:59 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,7 +20,9 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  Movie.new(attributes)
+  movie = Movie.new(attributes)
+  movie.save
+  movie
 end
 
 def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
