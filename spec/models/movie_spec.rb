c78ec4c8bbcf7e0e2698a5384416e1a9601f03d3
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-08 09:09:06 +0000

    Automatically backed up by Learn

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -65,7 +65,7 @@ describe 'Movie' do
       end
 
       it 'can be created with a hash of attributes' do
-        movie = can_be_created_with_a_hash_of_attributesW
+        movie = can_be_created_with_a_hash_of_attributes
         expect(Movie.find_by(attributes)).to eq(movie)
       end
 
