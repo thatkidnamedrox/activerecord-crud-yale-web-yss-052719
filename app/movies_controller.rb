a9057699581c7235d22d5d4cf58737a3e0c2e133
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-08 08:59:03 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -86,7 +86,6 @@ def can_update_multiple_items_at_once
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
   Movie.update_all(title: "A Movie")
-
 end
 
 def can_destroy_a_single_item
